e2d15e7546ad699dcb1e01baabb96915e0ac3647
wei chen <weichen9083@gmail.com>
2020-12-17 01:24:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side_3 = side_3
   end
 
-  def kind()
+  def kind
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
